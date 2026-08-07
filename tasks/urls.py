5caf04f2a5014e7ff19bfcab8703876feac66e90
Paul <paul.beltrand@inacapmail.cl>
2026-08-07 12:22:19 -0400

    bug: fix version include

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet, health
+from .views import TaskViewSet, health, version
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet, basename='task')
